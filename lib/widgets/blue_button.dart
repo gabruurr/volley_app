aac56ebf7154bf2b6126f4f388ff2c105c1f5cca
unknown <gabryelaraujo2019@gmail.com>
2025-05-01 13:40:18 -0300

    refactor: refatorado widget para botoes azuis

---
+++ lib/widgets/blue_button.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BlueButton extends StatelessWidget {
+  final double height;
+  final double widht;
+  final double offset;
+  final double fontSize;
+  final String label;
+  final Color foreground;
+  final Widget destiny;
+
+  const BlueButton({
+    super.key,
+    required this.height,
+    required this.widht,
+    required this.label,
+    required this.foreground,
+    required this.destiny,
+    required this.offset, required this.fontSize,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(horizontal: widht, vertical: height),
+        backgroundColor: Color.fromARGB(255, 43, 74, 142),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+          side: BorderSide(color: Colors.white),
+        ),
+      ),
+      onPressed: () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => destiny),
+        );
+      },
+      child: Transform.translate(
+        offset: Offset(0, offset),
+        child: Text(
+          label,
+          style: GoogleFonts.concertOne(
+            textStyle: TextStyle(fontSize: fontSize, color: foreground),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/home_widgets/start_button.dart
+++ lib/widgets/home_widgets/start_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:volley_app/screens/set.dart';
+import 'package:volley_app/widgets/blue_button.dart';
 
 class StartButton extends StatelessWidget {
   const StartButton({super.key});
@@ -16,27 +17,14 @@ class StartButton extends StatelessWidget {
             textStyle: TextStyle(fontSize: 25, color: Colors.white),
           ),
         ),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            padding: EdgeInsets.only(left: 55, right: 55, top: 4, bottom: 14),
-            backgroundColor: Color.fromARGB(255, 43, 74, 142),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8),
-              side: BorderSide(color: Colors.white),
-            ),
-          ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SetWidget()),
-            );
-          },
-          child: Text(
-            "Iniciar",
-            style: GoogleFonts.concertOne(
-              textStyle: TextStyle(fontSize: 30, color: Colors.white),
-            ),
-          ),
+        BlueButton(
+          height: 14,
+          widht: 55,
+          fontSize: 28,
+          label: "Iniciar",
+          foreground: Colors.white,
+          destiny: SetScreen(),
+          offset: -5,
         ),
       ],
     );
